de747db3ec26e97f7a4dfe9dcc4a0bd21ad6184e
erenvilla <ecv.eren@gmail.com>
2020-10-13 00:54:55 -0400

    corrected step function

--- src/dopeDogg.js
+++ src/dopeDogg.js
@@ -1,11 +1,12 @@
 var DopeDogg = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('dopedogg');
 };
 
 DopeDogg.prototype = Object.create(MakeDancer.prototype);
 DopeDogg.prototype.constructor = DopeDogg;
 
 DopeDogg.prototype.step = function() {
-  DopeDogg.prototype.step.call(this);
+  MakeDancer.prototype.step.call(this);
   this.$node.toggle();
 };
