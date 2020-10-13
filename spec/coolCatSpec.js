ac590a9844d48ad24fd54058f5612488a1a5ee6a
erenvilla <ecv.eren@gmail.com>
2020-10-13 15:10:07 -0400

    added test suites for coolCat and dopeDogg

---
+++ spec/coolCatSpec.js
@@ -0,0 +1,26 @@
+describe('coolCat', function() {
+
+  var coolCat, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    coolCat = new CoolCat(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(blinkyDancer, 'step');
+      expect(blinkyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps);
+      clock.tick(timeBetweenSteps);
+      expect(blinkyDancer.step.callCount).to.be.equal(1);
+      clock.tick(timeBetweenSteps);
+      expect(blinkyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
---
+++ spec/dopeDoggSpec.js
@@ -0,0 +1,26 @@
+describe('dopeDogg', function() {
+
+  var dopeDogg, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    dopeDogg = new MakeBlinkyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(dopeDogg.$node).to.be.an.instanceof(jQuery);
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(dopeDogg, 'step');
+      expect(dopeDogg.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps);
+      clock.tick(timeBetweenSteps);
+      expect(dopeDogg.step.callCount).to.be.equal(1);
+      clock.tick(timeBetweenSteps);
+      expect(dopeDogg.step.callCount).to.be.equal(2);
+    });
+  });
+});
