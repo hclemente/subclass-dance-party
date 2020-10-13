fd3fae55cbfeaf00ce5b69f07b5f34c96cb8dd87
erenvilla <ecv.eren@gmail.com>
2020-10-13 00:54:04 -0400

    Corrected step function

--- src/coolCat.js
+++ src/coolCat.js
@@ -1,11 +1,12 @@
 var CoolCat = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('coolcat');
 };
 
 CoolCat.prototype = Object.create(MakeDancer.prototype);
 CoolCat.prototype.constructor = CoolCat;
 
 CoolCat.prototype.step = function() {
-  CoolCat.prototype.step.call(this);
+  MakeDancer.prototype.step.call(this);
   this.$node.toggle();
 };
